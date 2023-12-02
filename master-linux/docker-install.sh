9c84cfbaff61b493c3890712e2e37126a11d42c1
brunoocana <brunoocana@gmail.com>
2023-12-02 12:09:57 -0300

    Setting up the Setups

---
+++ master-linux/docker-install.sh
@@ -0,0 +1,20 @@
+#Services and Docker
+
+# Add Docker's official GPG key:
+apt update
+apt install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install docker-compose ansible python3-pip -y
+
+mkdir -p /root/docker/website
+mkdir -p /root/docker/portainer
