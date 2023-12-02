b5714935fafbc7717aa7d7d1b686ad749a05bcf8
brunoocana <brunoocana@gmail.com>
2023-12-02 12:10:41 -0300

    Commiting

--- master-linux/basic-install.sh
+++ master-linux/basic-install.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
 apt update
-apt install resolvconf ntpd vim ssh python3 -y
+apt install resolvconf net-tools ntpd vim curl ssh python3 -y
 systemctl enable ntpd; systemctl start ntpd
 
 hostnamectl set-hostname Master-Linux
-echo time.google.com >> /etc/ntp.conf; echo time.windows.com >> /etc/ntp.conf
+echo time.google.com >> /etc/ntp.conf && echo time.windows.com >> /etc/ntp.conf
 timedatectl set-timezone UTC
 timedatectl set-timezone America/Sao_Paulo
 timedatectl set-ntp true
 
+ip -c a
 $ipaddress = ip address show | grep inet | awk '{print $2}'
 
-touch configuring-ip.py
-cat <<EOF>> configuring-ip.py
+cat <<EOF>> /etc/network/interfaces
 auto ens33
 #iface ens33 inet dhcp
 iface ens33 inet static
 address  /24
 gateway 
-dns-nameservers
+dns-nameservers 8.8.8.8 1.1.1.1
 EOF
+
+echo "nameserver 127.0.0.1" >> /etc/resolvconf/resolv.conf.d/tail
+echo "nameserver 8.8.8.8" >> /etc/resolvconf/resolv.conf.d/tail
+echo "nameserver 1.1.1.1" >> /etc/resolvconf/resolv.conf.d/tail
+
+nano /etc/network/interfaces
+systemctl restart networking.service
+
+
