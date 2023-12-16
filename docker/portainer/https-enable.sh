3d0b40fb362c2548221e710f515d6449d9d0db26
brunoocana <brunoocana@gmail.com>
2023-12-16 10:26:19 -0300

    Correction

--- docker/portainer/https-enable.sh
+++ docker/portainer/https-enable.sh
@@ -4,7 +4,7 @@ apt install openssl
 
 # Gerando o certificado na pasta "certs"
 openssl genrsa -out ./certs/portainer.key 2048
-openssl req -new -key portainer.key -out ./certs/portainer.csr
+openssl req -new -key ./certs/portainer.key -out ./certs/portainer.csr
 openssl x509 -req -days 365 -in ./certs/portainer.csr -signkey ./certs/portainer.key -out ./certs/portainer.crt
 
 # Criando o volume no docker
